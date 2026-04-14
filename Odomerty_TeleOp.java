a14a1ba7a7e862e7c2fc51f7832e199fcb815cc8
aSlicedOrange <terkahubert@gmail.com>
2026-04-14 20:02:46 +0100

    Complete Odomerty_TeleOp implementation

--- Odomerty_TeleOp.java
+++ Odomerty_TeleOp.java
@@ -1,9 +1,16 @@
 /* 
 ---------------------------------------
-File isnt finished yet
+File is finished
 
-Work in Progress
+Needs values to be put in
+
+Needs gobilda repository
 ---------------------------------------
+*/
+
+/*
+It needs the gobilda github repository to work
+This is field centric drive teleop with the odometry pods
 */
 
 
@@ -23,14 +30,14 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
 
 @TeleOp(name = "Odometry Opmode File")
-public class Odomerty_TeleOp extends OpMode{
+public class Odomerty_TeleOp extends OpMode {
 
     GoBildaPinpointDriver odo; //Get the gobilda repository
 
-    private DcMotor leftFrontMotor;
-    private DcMotor rightFrontMotor;
-    private DcMotor leftBackMotor;
-    private DcMotor rightBackMotor;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
 
 
     @Override
@@ -50,4 +57,45 @@ public class Odomerty_TeleOp extends OpMode{
         odo.setOffsets( , ); //Offsets for where the opometry pods are on the robot
         odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWING_ARM); //Check if correct, for what hardware ur using
         odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD); //to tell encoder what positive direction is
+
+        odo.resetPosAndIMU();
+        Pose2D startingPosition = new Pose2D(DistanceUnit.MM, x, y, AngleUnit.RADIANS, 0)//Set the x and y values for the starting pos (depends on where you start) and heading
+        odo.setPosition(startingPosition)
+    }
+
+    public void moveRobot() {
+
+        double forward = -gamepad1.left_stick_y;
+        double strafe = -gamepad1.left_stick_x;
+        double rotate = -gamepad1.right_stick_x;
+
+        pose2D pos = odo.getPosition();
+        double heading = pos.getHeading(AngleUnit.RADIANS);
+
+        double cosAngle = Math.cos((Math.PI / 2) - heading);
+        double sinAngle = Math.sin((Math.PI / 2) - heading);
+
+        double globalStrafe = -forward * sinAngle + strafe * cosAngle;
+        double globalForward = forward * sinAngle + strafe * cosAngle;
+
+        //Array for drive motors
+        double[] newWheelSpeeds = new double[4];
+
+        newWheelSpeeds[0] = globalForward + globalStrafe + rotate;
+        newWheelSpeeds[1] = globalForward - globalStrafe - rotate;
+        newWheelSpeeds[2] = globalForward - globalStrafe + rotate;
+        newWheelSpeeds[3] = globalForward + globalStrafe - rotate;
+
+        frontLeft.setPower(newWheelSpeeds[0]);
+        frontRight.setPower(newWheelSpeeds[1]);
+        backLeft.setPower(newWheelSpeeds[2]);
+        backRight.setPower(newWheelSpeeds[3]);
+    }
+
+    @Override
+    public void loop() {
+        moveRobot();
+
+        //Gets the new position from pods every loop
+        Pose2D pos = odo.getPosition();
     }
